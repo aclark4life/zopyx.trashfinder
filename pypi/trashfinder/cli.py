10575e4315f0f230fc2df3c3cf368231a335e84b
Alex Clark <aclark@aclark.net>
2011-11-23 19:49:37 -0500

    Make it so we can wrap in html

--- pypi/trashfinder/cli.py
+++ pypi/trashfinder/cli.py
@@ -4,17 +4,17 @@ from xmlrpclib import Server
 server = Server('http://pypi.python.org/pypi')
 
 def process_packages(packages):
-    results = ''
+    results = []
 
     num_packages = len(packages)
 
     for i, package in enumerate(packages):
-        results += 'Processing %r (%d/%d)' % (package, i+1, num_packages)
+        results.append( 'Processing %r (%d/%d)' % (package, i+1, num_packages))
 
         versions = server.package_releases(package)
         versions.sort()
         for version in versions:
-            results += '   %s' % str(version)
+            results.append( '   %s' % str(version))
             urls = server.release_urls(package, version)
             metadata = server.release_data(package, version)
 
@@ -24,32 +24,32 @@ def process_packages(packages):
                 have_sdist = False
                 for url in urls:
                     url = url['url']
-                    results += '   %s' % str(url)
+                    results.append( '   %s' % str(url))
                     if url.endswith('.bz2') or url.endswith('.zip') or url.endswith('gz'):
                         have_sdist = True
                     if url.endswith('egg'):
                         have_eggs = True
 
                 if have_eggs and not have_sdist:
-                    results += 'CRAP: %s has only egg release files but no sdist release file' % package
+                    results.append( 'CRAP: %s has only egg release files but no sdist release file' % package)
 
                 if have_eggs and have_sdist:
-                    results += 'CRAP(possible): %s has egg *and* sdist release file' % package
+                    results.append( 'CRAP(possible): %s has egg *and* sdist release file' % package)
 
             # externally hosted packages
             else:
                 download_url = metadata['download_url']
                 if download_url == 'UNKNOWN':
-                    results += 'CRAP: %s==%s - no release files, no valid download_url' % (package, version)
+                    results.append( 'CRAP: %s==%s - no release files, no valid download_url' % (package, version))
 
             if len(metadata['description'] or '') < 40:
-                    results += 'CRAP: %s==%s - description < 40 chars' % (package, version)
+                    results.append( 'CRAP: %s==%s - description < 40 chars' % (package, version))
             if len(metadata['summary'] or '') < 10:
-                    results += 'CRAP: %s==%s - summary < 10 chars' % (package, version)
+                    results.append( 'CRAP: %s==%s - summary < 10 chars' % (package, version))
             if not metadata['author_email'] and not metadata['maintainer_email']:
-                    results += 'CRAP: %s==%s - no author and no maintainer email given' % (package, version)
+                    results.append( 'CRAP: %s==%s - no author and no maintainer email given' % (package, version))
             if not metadata['author'] and not metadata['maintainer']:
-                    results += 'CRAP: %s==%s - no author and no maintainer name given' % (package, version)
+                    results.append( 'CRAP: %s==%s - no author and no maintainer name given' % (package, version))
 
     return results
 
@@ -64,7 +64,8 @@ def main():
         packages = [p for p in packages if p.startswith(prefix)]
 
     results = process_packages(packages)
-    print results
+    for line in results:
+        print line
 
 
 if __name__ == '__main__':
