9844be9346f2789486d7f17214303bb6270b4914
Alex Clark <aclark@aclark.net>
2012-02-25 21:44:37 -0500

    Remove references to "CRAP"

--- pypi/trashfinder/cli.py
+++ pypi/trashfinder/cli.py
@@ -31,25 +31,25 @@ def process_packages(packages):
                         have_eggs = True
 
                 if have_eggs and not have_sdist:
-                    results.append( 'CRAP: %s has only egg release files but no sdist release file' % package)
+                    results.append( '%s has only egg release files but no sdist release file' % package)
 
                 if have_eggs and have_sdist:
-                    results.append( 'CRAP(possible): %s has egg *and* sdist release file' % package)
+                    results.append( '%s has egg *and* sdist release file' % package)
 
             # externally hosted packages
             else:
                 download_url = metadata['download_url']
                 if download_url == 'UNKNOWN':
-                    results.append( 'CRAP: %s==%s - no release files, no valid download_url' % (package, version))
+                    results.append( '%s==%s - no release files, no valid download_url' % (package, version))
 
             if len(metadata['description'] or '') < 40:
-                    results.append( 'CRAP: %s==%s - description < 40 chars' % (package, version))
+                    results.append( '%s==%s - description < 40 chars' % (package, version))
             if len(metadata['summary'] or '') < 10:
-                    results.append( 'CRAP: %s==%s - summary < 10 chars' % (package, version))
+                    results.append( '%s==%s - summary < 10 chars' % (package, version))
             if not metadata['author_email'] and not metadata['maintainer_email']:
-                    results.append( 'CRAP: %s==%s - no author and no maintainer email given' % (package, version))
+                    results.append( '%s==%s - no author and no maintainer email given' % (package, version))
             if not metadata['author'] and not metadata['maintainer']:
-                    results.append( 'CRAP: %s==%s - no author and no maintainer name given' % (package, version))
+                    results.append( '%s==%s - no author and no maintainer name given' % (package, version))
 
     return results
 
