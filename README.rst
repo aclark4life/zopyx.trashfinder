169d001781d087790b258c3da2df0622e78ad188
Alex Clark <aclark@aclark.net>
2012-03-15 21:09:43 -0400

    Add metadata, readme

---
+++ README.rst
@@ -0,0 +1,6 @@
+Introduction
+============
+
+This is a fork of `zopyx.trashfinder`_, with modifications made to make integration with pythonpackages.com easier.
+
+_`zopyx.trashfinder`: http://pythonpackages.com/package/zopyx.trashfinder
--- setup.py
+++ setup.py
@@ -1,5 +1,11 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages
+from setuptools import setup
 
 setup(
     name='pypi.trashfinder',
+    namespace_packages=[
+        'pypi',
+        ],
+    packages=find_packages(),
+    version='0.0.1',
 )
