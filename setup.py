a58c2a5a496773e0c5f4db5aeb8f493fcce43668
Alex Clark <aclark@aclark.net>
2012-03-15 21:21:59 -0400

    Add meta

--- setup.py
+++ setup.py
@@ -2,10 +2,11 @@ from setuptools import find_packages
 from setuptools import setup
 
 setup(
+    description='This is a fork of zopyx.trashfinder, with modifications made to make integration with pythonpackages.com easier. It is not intended to be used outside of pythonpackages.com. For a command line trash finder, please install zopyx.trashfinder.',
     name='pypi.trashfinder',
     namespace_packages=[
         'pypi',
         ],
     packages=find_packages(),
-    version='0.0.1',
+    version='0.0.0',
 )
