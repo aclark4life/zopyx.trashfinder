d2562b224c191d4b69f7e7e25b155f8d441bd82e
Alex Clark <aclark@aclark.net>
2012-04-25 17:44:47 -0400

    Only check the most recent release

--- pypi/trashfinder/cli.py
+++ pypi/trashfinder/cli.py
@@ -1,48 +1,33 @@
-import sys
 from xmlrpclib import Server
-
 server = Server('http://pypi.python.org/pypi')
+
 
 def process_packages(packages):
     results = []
-
-    num_packages = len(packages)
-
     for i, package in enumerate(packages):
-        #results.append( 'Processing %r (%d/%d)' % (package, i+1, num_packages))
-
         versions = server.package_releases(package)
         versions.sort()
+        if len(versions) > 0:
+            version = 
         for version in versions:
-            #results.append( '   %s' % str(version))
             urls = server.release_urls(package, version)
             metadata = server.release_data(package, version)
-
-            # PyPI hosted packages
-            if urls:
+            if urls:  # PyPI hosted packages
                 have_eggs = False
                 have_sdist = False
                 for url in urls:
                     url = url['url']
-                    #results.append( '   %s' % str(url))
                     if url.endswith('.bz2') or url.endswith('.zip') or url.endswith('gz'):
                         have_sdist = True
                     if url.endswith('egg'):
                         have_eggs = True
-
                 if have_eggs and not have_sdist:
                     results.append( '%s has only egg release files but no sdist release file' % package)
-
-                #if have_eggs and have_sdist:
-                #    results.append( '%s has egg *and* sdist release file' % package)
-
-            # externally hosted packages
-            else:
+            else:  # externally hosted packages
                 download_url = metadata['download_url']
                 if download_url == 'UNKNOWN':
                     results.append( '%s - No release files, no valid download_url' %
                         version)
-
             if len(metadata['description'] or '') < 40:
                     results.append( '%s - description < 40 chars' % version)
             if len(metadata['summary'] or '') < 10:
@@ -53,23 +38,6 @@ def process_packages(packages):
             if not metadata['author'] and not metadata['maintainer']:
                     results.append( '%s - No author and no maintainer name given' %
                         version)
-
+    if results == list():
+        print '%s is OK' % version
     return results
-
-def main():
-
-    prefix = ''
-    if len(sys.argv) > 1:
-        prefix = sys.argv[1]
-
-    packages = server.list_packages()
-    if prefix:
-        packages = [p for p in packages if p.startswith(prefix)]
-
-    results = process_packages(packages)
-    for line in results:
-        print line
-
-
-if __name__ == '__main__':
-    main()
