a4a916a17d3cde60f8f821bc1a2fefaedb8622cb
Alex Clark <aclark@aclark.net>
2012-04-25 17:37:42 -0400

    Require setuptools

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ from setuptools import setup
 
 setup(
     description='This is a fork of zopyx.trashfinder, with modifications made to make integration with pythonpackages.com easier. It is not intended to be used outside of pythonpackages.com. For a command line trash finder, please install zopyx.trashfinder.',
+    install_requires=[
+        'setuptools',
+    ],
     name='pypi.trashfinder',
     namespace_packages=[
         'pypi',
